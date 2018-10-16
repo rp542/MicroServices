7b63918ff75411c4efb8ef3c45d1d3d45ff5f927
Rameshwarambandi <priyanka.rameshwarambandi@capgemini.com>
2018-10-16 08:51:57 -0400

    commit

---
+++ eurekaserverr/eurekaserverr/src/main/java/com/capgemini/eurekaserverr/EurekaserverrApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.eurekaserverr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverrApplication.class, args);
+	}
+}
